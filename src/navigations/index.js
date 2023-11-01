2d94e7efb6911eb3f31a1a5b08cf394607ae9c0e
rohitthakaret <rohitthakaret@gmail.com>
2023-11-01 11:20:54 +0530

    Splash screen Done

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from '../screens/HomeScreen';
+import WellcomeScreen from "../screens/WellcomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 function AppNavigation(){
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+            <Stack.Navigator initialRouteName="Wellcome" screenOptions={{headerShown:false}}>
             <Stack.Screen name="Home" component={HomeScreen}/>
+            <Stack.Screen name="Wellcome" component={WellcomeScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
