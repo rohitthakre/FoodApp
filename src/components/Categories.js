e36dd2c568f62913a50719b2d7b222c782b82cb5
rohitthakaret <rohitthakaret@gmail.com>
2023-11-01 20:51:16 +0530

    Category and recipes

---
+++ src/components/Categories.js
@@ -0,0 +1,49 @@
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View,Image } from 'react-native'
+import React from 'react'
+import { categoryData } from '../constants';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Animated,{ FadeInDown } from 'react-native-reanimated';
+
+
+const Categories = ({ categories, activeCategory, handleChangesCategory }) => {
+  return (
+    <Animated.View entering={FadeInDown.duration(500).springify()}>
+        <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{}}
+        contentContainerStyle={{paddingHorizontal:15}}
+        >
+            {
+                categories.map((cat,index)=>{
+
+                    let isActive = cat.strCategory === activeCategory;
+                    let activeButtonClass = isActive ? '#ff944d' :'#d3d3d3'
+
+                    return(
+                        <TouchableOpacity
+                        key={index}
+                            onPress={() => handleChangesCategory(cat.strCategory)}
+                        style={{flex:1,alignItems:"center",justifyContent:"center",marginLeft:10}}
+                        >
+                            <View style={{padding:10,backgroundColor:activeButtonClass,borderRadius:hp(6)}}>
+                                <Image source={{uri:cat.strCategoryThumb}}  style={{width:hp(6),
+                                height:hp(6),
+                                borderRadius:hp(6)
+                                }}
+                                // resizeMode='contain'
+                                />
+                            </View>
+                            <Text style={{fontSize:hp(1.6)}}>{cat.strCategory}</Text>
+                        </TouchableOpacity>
+                    )
+                })
+            }
+        </ScrollView>
+    </Animated.View>
+  )
+}
+
+export default Categories
+
+const styles = StyleSheet.create({})
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const categoryData = [
+    {
+        "idCategory": "1",
+        "strCategory": "Beef",
+        "strCategoryThumb": "https://www.themealdb.com/images/category/beef.png"
+    },
+    {
+        "idCategory": "2",
+        "strCategory": "Chicken",
+        "strCategoryThumb": "https://www.themealdb.com/images/category/chicken.png"
+    },
+    {
+        "idCategory": "3",
+        "strCategory": "Dessert",
+        "strCategoryThumb": "https://www.themealdb.com/images/category/dessert.png",
+    },
+    {
+        "idCategory": "4",
+        "strCategory": "Lamb",
+        "strCategoryThumb": "https://www.themealdb.com/images/category/lamb.png"
+    }
+]
